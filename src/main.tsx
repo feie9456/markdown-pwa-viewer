b5a2be0d66de1d712e42937176fcd6ac2cdf4c5a
feie9456 <feie9454@stmail.ujs.edu.cn>
2026-08-21 11:31:54 +0800

    feat: add React entrypoint

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
+import App from './App'
+import './styles.css'
+import 'katex/dist/katex.min.css'
+import 'highlight.js/styles/github.css'
+
+registerSW({ immediate: true })
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
